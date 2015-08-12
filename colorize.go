e1128a35ae9b46d94b465198d359ca4af91beb94
Shaba Abhiram <shaba@ottoq.com>
2015-08-11 22:11:42 -0700

    Fix lint errors, comments et al

--- colorize.go
+++ colorize.go
@@ -1,5 +1,4 @@
-// "colorize" is a simple package which returns an ascii colorized
-// string version of an input string
+// Package colorize returns an ascii colorized string version of an input string
 //
 // Here is a table of ASCII to color values:
 //
@@ -12,60 +11,48 @@
 package colorize
 
 import (
-    "fmt"
-    "regexp"
-    "strings"
+	"fmt"
+	"regexp"
+	"strings"
 )
 
-// Define a map which can convert the string values of an input ASCII
+// colorToValueMap can convert the string values of an input ASCII
 // color to its appropriate integer counterpart. See table above for
 // mapping information
-var colorToValueMap = map [string] int {
-    "black":   0,
-    "red":     1,
-    "green":   2,
-    "yellow":  3,
-    "blue":    4,
-    "magenta": 5,
-    "cyan":    6,
-    "white":   7,
+var colorToValueMap = map[string]int{
+	"black":   0,
+	"red":     1,
+	"green":   2,
+	"yellow":  3,
+	"blue":    4,
+	"magenta": 5,
+	"cyan":    6,
+	"white":   7,
 }
 
-// Function which returns the "input" string after sorrounding it
-// by the appropriate ASCII escape sequence for the requested "color".
+// ColorString is a function which returns the "input" string after surrounding
+// it by the appropriate ASCII escape sequence for the requested "color".
 // If the "color" input is not a member of the map, we simply return
 // the input without any change
 func ColorString(input, color string) string {
-    color = strings.ToLower(color)
-    if colorIndex, valid := colorToValueMap[color]; valid {
-        return fmt.Sprintf("\033[3%dm%s\033[0m", colorIndex, input)
-    }
-    return input
+	color = strings.ToLower(color)
+	if colorIndex, valid := colorToValueMap[color]; valid {
+		return fmt.Sprintf("\033[3%dm%s\033[0m", colorIndex, input)
+	}
+	return input
 }
 
-// Function which takes in a string embedded with "<color>...</color>" tags.
-// Any un-matched tags will be ignored and not stripped from the input string
+// Colorize is a function whichtakes in a string embedded with
+// "<color>...</color>" tags. Any un-matched tags will be ignored and not
+// stripped from the input string
 func Colorize(input string) string {
-    for color, index := range colorToValueMap {
-        var (
-            searchString  = fmt.Sprintf("(<%s>)(.*?)(</%s>)", color, color)
-            replaceString = fmt.Sprintf("\033[3%dm$2\033[0m", index)
-            match         = regexp.MustCompile(searchString)
-        )
-        input = match.ReplaceAllString(input, replaceString)
-    }
-    return input
+	for color, index := range colorToValueMap {
+		var (
+			searchString  = fmt.Sprintf("(<%s>)(.*?)(</%s>)", color, color)
+			replaceString = fmt.Sprintf("\033[3%dm$2\033[0m", index)
+			match         = regexp.MustCompile(searchString)
+		)
+		input = match.ReplaceAllString(input, replaceString)
+	}
+	return input
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
--- colorize_test.go
+++ colorize_test.go
@@ -2,78 +2,79 @@
 package colorize
 
 import (
-    "testing"
-    "fmt"
+	"fmt"
+	"testing"
 )
 
-// Validate "colorize.ColorString()"
+// TestColorString validates the ColorString function
 func TestColorString(test *testing.T) {
-    // Define our test cases for the colorize with string color cases
-    cases := [] struct {
-        input, color, expected string
-    }{
-        // Non-standard cases
-        { "Test", "BLACK",   "\x1b[30mTest\x1b[0m" },
-        { "Test", "Black",   "\x1b[30mTest\x1b[0m" },
-        { "Test", "INVALID", "Test" },
-        { "Test", "", "Test" },
+	// Define our test cases for the colorize with string color cases
+	cases := []struct {
+		input, color, expected string
+	}{
+		// Non-standard cases
+		{"Test", "BLACK", "\x1b[30mTest\x1b[0m"},
+		{"Test", "Black", "\x1b[30mTest\x1b[0m"},
+		{"Test", "INVALID", "Test"},
+		{"Test", "", "Test"},
 
-        // Typical usage
-        { "Test", "black",   "\x1b[30mTest\x1b[0m" },
-        { "Test", "red",     "\x1b[31mTest\x1b[0m" },
-        { "Test", "green",   "\x1b[32mTest\x1b[0m" },
-        { "Test", "yellow",  "\x1b[33mTest\x1b[0m" },
-        { "Test", "blue",    "\x1b[34mTest\x1b[0m" },
-        { "Test", "magenta", "\x1b[35mTest\x1b[0m" },
-        { "Test", "cyan",    "\x1b[36mTest\x1b[0m" },
-        { "Test", "white",   "\x1b[37mTest\x1b[0m" },
-    }
+		// Typical usage
+		{"Test", "black", "\x1b[30mTest\x1b[0m"},
+		{"Test", "red", "\x1b[31mTest\x1b[0m"},
+		{"Test", "green", "\x1b[32mTest\x1b[0m"},
+		{"Test", "yellow", "\x1b[33mTest\x1b[0m"},
+		{"Test", "blue", "\x1b[34mTest\x1b[0m"},
+		{"Test", "magenta", "\x1b[35mTest\x1b[0m"},
+		{"Test", "cyan", "\x1b[36mTest\x1b[0m"},
+		{"Test", "white", "\x1b[37mTest\x1b[0m"},
+	}
 
-    // Run tests
-    for _, tc := range cases {
-        actual := ColorString(tc.input, tc.color)
-        if actual != tc.expected {
-            test.Errorf("ColorString(%q, %q) == %q, expected %q",
-                        tc.input, tc.color, actual, tc.expected)
-        }
-    }
+	// Run tests
+	for _, tc := range cases {
+		actual := ColorString(tc.input, tc.color)
+		if actual != tc.expected {
+			test.Errorf("ColorString(%q, %q) == %q, expected %q",
+				tc.input, tc.color, actual, tc.expected)
+		}
+	}
 }
 
-// Validate "colorize.Colorize()"
+// TestColorize validates that the Colorize function works
 func TestColorize(test *testing.T) {
-    // Define our test cases for the colorize with string color cases
-    cases := [] struct {
-        input, expected string
-    }{
-        { "Test <red>A</red> <blue>B</blue>", "Test \x1b[31mA\x1b[0m \x1b[34mB\x1b[0m" },
-        { "Test <red>A</red> <red>B</red>",   "Test \x1b[31mA\x1b[0m \x1b[31mB\x1b[0m" },
-    }
+	// Define our test cases for the colorize with string color cases
+	cases := []struct {
+		input, expected string
+	}{
+		{"Test <red>A</red> <blue>B</blue>", "Test \x1b[31mA\x1b[0m \x1b[34mB\x1b[0m"},
+		{"Test <red>A</red> <red>B</red>", "Test \x1b[31mA\x1b[0m \x1b[31mB\x1b[0m"},
+	}
 
-    // Run tests
-    for _, tc := range cases {
-        actual := Colorize(tc.input)
-        if actual != tc.expected {
-            test.Errorf("Colorize(%q) == %q, expected %q",
-                        tc.input, actual, tc.expected)
-        }
-    }
+	// Run tests
+	for _, tc := range cases {
+		actual := Colorize(tc.input)
+		if actual != tc.expected {
+			test.Errorf("Colorize(%q) == %q, expected %q",
+				tc.input, actual, tc.expected)
+		}
+	}
 }
 
-
+// ExampleColorString provides sample usage
 func ExampleColorString() {
-    fmt.Println("Hello - " + ColorString("black",   "black"))
-    fmt.Println("Hello - " + ColorString("red",     "red"))
-    fmt.Println("Hello - " + ColorString("green",   "green"))
-    fmt.Println("Hello - " + ColorString("yellow",  "yellow"))
-    fmt.Println("Hello - " + ColorString("blue",    "blue"))
-    fmt.Println("Hello - " + ColorString("magenta", "magenta"))
-    fmt.Println("Hello - " + ColorString("cyan",    "cyan"))
-    fmt.Println("Hello - " + ColorString("white",   "white"))
+	fmt.Println("Hello - " + ColorString("black", "black"))
+	fmt.Println("Hello - " + ColorString("red", "red"))
+	fmt.Println("Hello - " + ColorString("green", "green"))
+	fmt.Println("Hello - " + ColorString("yellow", "yellow"))
+	fmt.Println("Hello - " + ColorString("blue", "blue"))
+	fmt.Println("Hello - " + ColorString("magenta", "magenta"))
+	fmt.Println("Hello - " + ColorString("cyan", "cyan"))
+	fmt.Println("Hello - " + ColorString("white", "white"))
 
 }
 
+// ExampleColorize provides sample usage
 func ExampleColorize() {
-    fmt.Println(Colorize(`
+	fmt.Println(Colorize(`
 
 
 <red>This text will be red</red> and this is default...
